1021483685482ca82228819773af57ee8b2f3f06
M3MONs <alan.skrrrz@gmail.com>
2021-09-15 21:00:26 +0200

    change header href

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
     return (
         <header>
-            <a href="/#"><h1>The<strong>Anime</strong>DataBase</h1></a>
+            <a href="/"><h1>The<strong>Anime</strong>DataBase</h1></a>
             <hr />
         </header>
     )
