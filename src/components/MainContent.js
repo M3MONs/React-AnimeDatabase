516cac7059af68691302d839d302429aa03517e9
M3MONs <alan.skrrrz@gmail.com>
2022-12-09 14:25:28 +0100

    upgraded for jikan v4

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,28 +1,25 @@
 import React from "react";
 import AnimeCard from "./AnimeCard";
 
-function MainContent(props) {
-  return (
-    <main>
-      <div className="main-header">
-        <h2>Search Anime</h2>
-        <form className="search-box" onSubmit={props.HandleSearch}>
-          <input
-            type="text"
-            placeholder="Search for an anime..."
-            required
-            value={props.search}
-            onChange={(e) => props.setSearch(e.target.value)}
-          />
-        </form>
-      </div>
-      <div className="anime-list">
-        {props.animeList.map((anime) => (
-          <AnimeCard key={anime.mal_id} anime={anime} />
-        ))}
-      </div>
-    </main>
-  );
+function MainContent({ animeList, search }) {
+    const handleDefault = (e) => {
+        e.preventDefault();
+    };
+    return (
+        <main>
+            <div className='main-header'>
+                <h2>Search Anime</h2>
+                <form className='search-box' onSubmit={handleDefault}>
+                    <input type='text' placeholder='Search for an anime...' onChange={search} />
+                </form>
+            </div>
+            <div className='anime-list'>
+                {animeList.map((anime) => (
+                    <AnimeCard key={anime.mal_id} anime={anime} />
+                ))}
+            </div>
+        </main>
+    );
 }
 
 export default MainContent;
